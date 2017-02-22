067d26047bfede5c2ba5b5da00c7f903344a886e
Mehri Russo <mehrimova@gmail.com>
2017-02-22 11:50:16 -0700

    finished object exercise

--- main.js
+++ main.js
@@ -60,8 +60,7 @@ function Husky() {
 }
 
 // TODO: Use Object.create() in order to extend the superclass with the subclass
-//write code here
-
+Husky.prototype = Object.create(Dog.prototype);
 var fido = new Husky();
 
 console.log('Is fido an instance of Husky?', fido instanceof Husky);// should print true
